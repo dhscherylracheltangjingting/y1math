91df6d933ddb401388158f6b5cecf84ea1806337
dhscherylracheltangjingting <37992174+dhscherylracheltangjingting@users.noreply.github.com>
2019-10-18 14:30:13 +0800

    Update t01primeshcflcm_hcf1.py

--- t01primeshcflcm_hcf1.py
+++ t01primeshcflcm_hcf1.py
@@ -1,2 +1,13 @@
-# Find the Highest Common Factor (HCF) of 18 and 30.
+# Find the Highest Common Factor (HCF) of 18 and 30
+
+# Find the numbers of 18 
+
+num = 18 
+factors = []
+
+# Run a For Loop, running numbers from 2 to 18
+for i in range(2,num+1):
+
+  if num % i == 0
+
 
